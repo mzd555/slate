836d8cdcfc4b05f5296216e1367d3087fb415dad
Ihor Omelchenko <megapixel23@gmail.com>
2018-01-15 21:44:31 +0200

    Sandbox deployment added

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 if [ "$1" == "sandbox" ]
 then
-    1
-#  npm run dev
-#  aws s3 sync ./dist/ s3://docs.deliverysolutions.co
-#  aws cloudfront create-invalidation --distribution-id EBVQ883DRQWKB --paths /index.html
+    vagrant ssh -c "cd /vagrant && bundle exec middleman build --clean"
+    aws s3 sync ./build/ s3://docs.deliverysolutions.co
+    aws cloudfront create-invalidation --distribution-id EBVQ883DRQWKB --paths /index.html
 else
-  vagrant ssh -c "cd /vagrant && bundle exec middleman build --clean"
-  aws s3 sync ./build/ s3://development.docs.deliverysolutions.co
+    vagrant ssh -c "cd /vagrant && bundle exec middleman build --clean"
+    aws s3 sync ./build/ s3://development.docs.deliverysolutions.co
 fi
